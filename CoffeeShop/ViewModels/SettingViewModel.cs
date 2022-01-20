9ca905fe2ccaf73d9693a40672aa2b816453b5b0
danhnc2k <55302957+danhnc2k@users.noreply.github.com>
2022-01-21 00:05:30 +0700

    Fix conflict settingVM

--- CoffeeShop/ViewModels/SettingViewModel.cs
+++ CoffeeShop/ViewModels/SettingViewModel.cs
@@ -1,15 +1,17 @@
 ﻿using CoffeeShop.Model;
+using CoffeeShop.Services;
+using CoffeeShop.Services.ModelServices;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using CoffeeShop.Services.ModelServices;
 
 namespace CoffeeShop.ViewModels
 {
-    public class SettingViewModel:BaseViewModel
+    public class SettingViewModel : BaseViewModel
     {
         #region variables
         private static SettingViewModel _instance = null;
@@ -46,11 +48,17 @@ namespace CoffeeShop.ViewModels
         public string PasswordOld { get => _passwordOld; set { _passwordOld = value; OnPropertyChanged(); } }
         public string PasswordNew { get => _passwordNew; set { _passwordNew = value; OnPropertyChanged(); } }
         public string PasswordNew2 { get => _passwordNew2; set { _passwordNew2 = value; OnPropertyChanged(); } }
-        public bool IsOpenChangePasswordDialog { get => _isOpenChangePasswordDialog; set { _isOpenChangePasswordDialog = value;
+        public bool IsOpenChangePasswordDialog
+        {
+            get => _isOpenChangePasswordDialog; set
+            {
+                _isOpenChangePasswordDialog = value;
                 PasswordOld = null;
                 PasswordNew = null;
                 PasswordNew2 = null;
-                OnPropertyChanged(); } }
+                OnPropertyChanged();
+            }
+        }
 
         // thông số
         public float TLDDKHT { get => _tLDDKHT; set { _tLDDKHT = value; OnPropertyChanged(); } }
@@ -68,6 +76,10 @@ namespace CoffeeShop.ViewModels
         public ICommand ClickChangePasswordButtonCommand { get; set; }
         public ICommand CloseChangePasswordDialog { get; set; }
         public ICommand ClickUpdateButtonCommand { get; set; }
+        public ICommand ClickCurrentEventButtonCommand { get; set; }
+        public ICommand ClickAddEventButtonCommand { get; set; }
+        public ICommand ClickSaveEventButtonCommand { get; set; }
+        public ICommand ClickDeleteEventButtonCommand { get; set; }
 
         #endregion
         public static SettingViewModel GetInstance()
@@ -89,7 +101,7 @@ namespace CoffeeShop.ViewModels
         public SettingViewModel()
         {
             // khởi tạo dữ liệu
-            TLDDKHT = float.Parse(ThongSoService.GetByName("TiLeDoiDiemKHThuong").GiaTri)*10000;
+            TLDDKHT = float.Parse(ThongSoService.GetByName("TiLeDoiDiemKHThuong").GiaTri) * 10000;
             TLDDKHTX = float.Parse(ThongSoService.GetByName("TiLeDoiDiemKHThuongXuyen").GiaTri) * 10000;
             TLDDKHTT = float.Parse(ThongSoService.GetByName("TiLeDoiDiemKHThanThiet").GiaTri) * 10000;
             HMCTKHT = int.Parse(ThongSoService.GetByName("HanMucChiTieuKHThuong").GiaTri);
@@ -109,8 +121,8 @@ namespace CoffeeShop.ViewModels
             CloseChangePasswordDialog = new RelayCommand<object>((param) => { return true; }, (param) => {
                 if (bool.Parse(param.ToString()) == true)
                 {
-                    ThongSoService oldPassword = ThongSoService.GetByName("MaBaoMat");
-                    if (PasswordOld != oldPassword.GiaTri)
+                    ThongSoService mabaomat = ThongSoService.GetByName("MaBaoMat");
+                    if (PasswordOld != mabaomat.GiaTri)
                     {
                         Message = "Mật khẩu cũ không chính xác";
                         IsOpenMessageDialog = true;
@@ -124,12 +136,8 @@ namespace CoffeeShop.ViewModels
                         }
                         else
                         {
-                            ThongSoService newPassword = new ThongSoService();
-                            newPassword.Ma = oldPassword.Ma;
-                            newPassword.Ten = oldPassword.Ten;
-                            newPassword.Kieu = oldPassword.Kieu;
-                            newPassword.GiaTri = PasswordNew;
-                            newPassword.Update();
+                            mabaomat.GiaTri = PasswordNew;
+                            mabaomat.Update();
                             IsOpenChangePasswordDialog = false;
                         }
                     }
@@ -152,12 +160,12 @@ namespace CoffeeShop.ViewModels
                     Message = "Tỉ lệ đổi điểm khách hàng thường xuyên không đúng";
                     IsOpenMessageDialog = true;
                 }
-                else if(float.Parse(TLDDKHTT.ToString()) < 0)
+                else if (float.Parse(TLDDKHTT.ToString()) < 0)
                 {
                     Message = "Tỉ lệ đổi điểm khách hàng thân thiết không đúng";
                     IsOpenMessageDialog = true;
                 }
-                else if(int.Parse(HMCTKHT.ToString()) < 0)
+                else if (int.Parse(HMCTKHT.ToString()) < 0)
                 {
                     Message = "Hạn mức chi tiêu khách hàng thường không đúng";
                     IsOpenMessageDialog = true;
@@ -179,29 +187,82 @@ namespace CoffeeShop.ViewModels
                 }
                 else
                 {
-
-                    SaveSetting("TiLeDoiDiemKHThuong", (TLDDKHT / 10000).ToString());
-                    SaveSetting("TiLeDoiDiemKHThuongXuyen", (TLDDKHTX / 10000).ToString());
-                    SaveSetting("TiLeDoiDiemKHThanThiet", (TLDDKHTT / 10000).ToString());
-                    SaveSetting("HanMucChiTieuKHThuong", HMCTKHT.ToString());
-                    SaveSetting("HanMucChiTieuKHThuongXuyen", HMCTKHTX.ToString());
-                    SaveSetting("HanMucChiTieuKHThanThiet", HMCTKHTT.ToString());
-                    SaveSetting("KyHanResetChiTieu", YearsReset.ToString());
+                    SaveConfig("TiLeDoiDiemKHThuong", (TLDDKHT / 10000).ToString());
+                    SaveConfig("TiLeDoiDiemKHThuongXuyen", (TLDDKHTX / 10000).ToString());
+                    SaveConfig("TiLeDoiDiemKHThanThiet", (TLDDKHTT / 10000).ToString());
+                    SaveConfig("HanMucChiTieuKHThuong", HMCTKHT.ToString());
+                    SaveConfig("HanMucChiTieuKHThuongXuyen", HMCTKHTX.ToString());
+                    SaveConfig("HanMucChiTieuKHThanThiet", HMCTKHTT.ToString());
+                    SaveConfig("KyHanResetChiTieu", YearsReset.ToString());
                     Message = "Cập nhật thành công";
                     IsOpenMessageDialog = true;
                 }
             });
 
+            ClickCurrentEventButtonCommand = new RelayCommand<dynamic>((param) => { return true; }, (param) => {
+                string[] eventArray = EventManager.AllCurrentEventList().ToArray();
+                if (eventArray.Length == 0)
+                {
+                    Alert("No event currently");
+                }
+                else
+                {
+                    Alert("List events:\n" + String.Join("\n", eventArray));
+                }
+            });
+
+            ClickAddEventButtonCommand = new RelayCommand<dynamic>((param) => { return true; }, (param) => {
+                OpenFileDialog dialog = new OpenFileDialog();
+                if (dialog.ShowDialog() == true)
+                {
+                    if (EventManager.LoadEventsFromFile(dialog.FileName))
+                    {
+                        Alert("Upload event success");
+                    }
+                    else
+                    {
+                        Alert("Upload event failed");
+                    }
+                }
+            });
+
+            ClickSaveEventButtonCommand = new RelayCommand<dynamic>((param) => { return true; }, (param) => {
+                if (EventManager.SaveToDefault())
+                {
+                    Alert("Save all current events to default success");
+                }
+                else
+                {
+                    Alert("Save all current events to default failed");
+                }
+            });
+
+            ClickDeleteEventButtonCommand = new RelayCommand<dynamic>((param) => { return true; }, (param) => {
+                if (EventManager.RemoveAllEvents())
+                {
+                    Alert("Remove events success");
+                }
+                else
+                {
+                    Alert("Remove events failed");
+                }
+            });
         }
-        private void SaveSetting(string name, string newValue)
+
+        private void SaveConfig(string settingName, string value)
         {
-            ThongSoService oldSetting = ThongSoService.GetByName(name);
-            ThongSoService newSetting = new ThongSoService();
-            newSetting.Ma = oldSetting.Ma;
-            newSetting.Ten = oldSetting.Ten;
-            newSetting.Kieu = oldSetting.Kieu;
-            newSetting.GiaTri = newValue;
-            newSetting.Update();
+            ThongSoService updatedSetting = ThongSoService.GetByName(settingName);
+            if (updatedSetting != null)
+            {
+                updatedSetting.GiaTri = value;
+                updatedSetting.Update();
+            }
+        }
+
+        private void Alert(string message)
+        {
+            Message = message;
+            IsOpenMessageDialog = true;
         }
     }
 }
