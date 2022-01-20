a0784d881ab67ad83c89823d65d78e74740d333a
danhnc2k <55302957+danhnc2k@users.noreply.github.com>
2022-01-20 22:40:34 +0700

    Update ReceiptVM

--- CoffeeShop/Services/Converters/DateConverter.cs
+++ CoffeeShop/Services/Converters/DateConverter.cs
@@ -14,7 +14,7 @@ namespace CoffeeShop.Services.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null || value == "") return "";
+            if (value == null || value.ToString() == "") return "";
             var tmp = value as Nullable<DateTime>;
             string res;
             res = $"{tmp.Value.Day}/{tmp.Value.Month}/{tmp.Value.Year}";
